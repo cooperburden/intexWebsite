200b4883175a1682db2f51217328967c54c25622
Cooper Burden <cooperburden@Coopers-MacBook-Pro-2.local>
2024-12-02 13:06:00 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const { Pool } = require('pg');
+
+let path = require("path");
+
+let security = false 
+
+const app = express();
+
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Middleware to parse URL-encoded data
+app.use(express.urlencoded({extended: true}));
+
+const port = 3010;
+
+// Route for the index.ejs file
+app.get('/', (req, res) => {
+    res.render('index'); // Render the index.ejs file
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
